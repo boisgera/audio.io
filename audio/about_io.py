c349a36a22a85457484c9ccfa244c5895e3e195d
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-19 22:15:43 +0100

    version 1.2.0

--- audio/about_io.py
+++ audio/about_io.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__        = "audio.io",
-  __version__     = "1.1",
+  __version__     = "1.2.0",
   __license__     = "MIT License",  
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__         = None,
